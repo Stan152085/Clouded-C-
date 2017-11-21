1044e53f69122c76344020feed8ef7a67d71bb54
Stan152085 <152085@ade-nhtv.nl>
2017-11-21 16:48:47 +0100

    added required dll's created small main loop to test if sfml is working removed 32 bit configuration

--- Clouded/Clouded/Source/Clouded.cpp
+++ Clouded/Clouded/Source/Clouded.cpp
@@ -2,10 +2,33 @@
 //
 
 #include "stdafx.h"
-
+#include "SFML/Main.hpp"
+#include "SFML/System.hpp"
+#include "SFML/Window.hpp"
 
 int main()
 {
+	sf::Window window;
+	sf::VideoMode mode(700, 500);
+
+	window.create(mode, "Clouded");
+
+	// game initialization
+
+	while (window.isOpen())
+	{
+		// put main gameloop here
+
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::EventType::Closed)
+			{
+				window.close();
+				break;
+			}
+		}
+	}
     return 0;
 }
 
