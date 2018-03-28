f5f21617146b331270b7ee2917eac5f8a775a454
Floris Kappeyne <floris.kappeyne@gmail.com>
2018-03-28 18:17:09 +0200

    * each mesh now has a transform, representing it's offset in the model

--- Clouded/Clouded/Source/Resources/AssetManager.h
+++ Clouded/Clouded/Source/Resources/AssetManager.h
@@ -8,10 +8,15 @@ class D3D11Renderer;
 // the handle to the model on the gpu
 struct GPUModel;
 
+// Rommie's transform class
+class Transform;
+
 // tinygltf's model
 namespace tinygltf
 {
+  class Node;
   class Model;
+  struct Primitive;
 }
 
 namespace resources
@@ -50,8 +55,12 @@ namespace resources
     AssetManager(const AssetManager& rhs);
     const AssetManager& operator=(const AssetManager& rhs);
 
-    // helper function to convert a tinygltf model into one of ours
+    // helper functions to convert a tinygltf model into one of ours
+    void RecursiveNodeExtract(const tinygltf::Model& source, const tinygltf::Node& node,
+      const std::vector<std::shared_ptr<Texture>>& textures, Model& result, Transform& l2w);
     void ConstructModel(const tinygltf::Model& source, Model& result, const std::string& model_name);
+    void ConstructMesh(const tinygltf::Model& source, const tinygltf::Primitive& prim,
+      const std::vector<std::shared_ptr<Texture>>& textures, Model& result, const Transform& l2w);
 
     using ModelMap = std::unordered_map<std::string, std::shared_ptr<GPUModel>>;
     using TextureMap = std::unordered_map<std::string, std::shared_ptr<resources::Texture>>;
